ca3440597ec3e545d06dc84cfd1662ad67979279
Hyeonkkyu <101560173+Hyeonkkyu@users.noreply.github.com>
2022-09-16 21:22:54 +0900

    22.09.16

---
+++ src/component/Footer.js
@@ -0,0 +1,26 @@
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            <div className="ft_top">
+                <div className="inner">
+                    <ul className="left">
+
+                    </ul>
+                    <div className="right">
+
+                    </div>
+                </div>
+            </div>
+            <div className="ft_bottom">
+                <div className="inner">
+                    <address className="left"></address>
+                    <div className="right">
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
